6b10c4acaf18871336e567e03b358d1336f43ab0
A. Chakravarty <abhichak17@gmail.com>
2023-09-03 22:25:59 +0100

    yeet

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-// JavaScript for typing effect
-const typedText = document.querySelector('.typed-text');
-const textArray = ["Abhirup Chakravarty"];
-let textIndex = 0;
-let charIndex = 0;
+const continueButton = document.getElementById('continue-button');
+const welcome = document.querySelector('.welcome');
 
-function type() {
-    if (charIndex < textArray[textIndex].length) {
-        typedText.textContent += textArray[textIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(type, 100); // Adjust typing speed here (milliseconds)
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(type, 1000); // Start typing after a delay (milliseconds)
-});
-
-// JavaScript for button action (slide up to portfolio)
-const openPortfolioButton = document.getElementById('open-portfolio');
-const portfolioSection = document.querySelector('.portfolio');
-
-openPortfolioButton.addEventListener('click', function () {
-    portfolioSection.scrollIntoView({ behavior: 'smooth' });
+continueButton.addEventListener('click', () => {
+    // Slide up animation
+    welcome.style.transform = 'translateY(-100%)';
+    // Show the section links
+    setTimeout(() => {
+        window.location.href = 'sections.html';
+    }, 500); // Adjust the delay as needed for the animation
 });
